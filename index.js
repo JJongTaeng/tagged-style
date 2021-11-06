40653b9e2d1fbb01de7b4355446aa6ddac26dad2
쫑탱 <jjojjo141995@gmail.com>
2021-11-06 22:45:09 +0900

    span, a, ul, li 추가

--- index.js
+++ index.js
@@ -22,6 +22,46 @@ class TaggedStyle {
     return $div;
   }
 
+  span(style, ...arg) {
+    const $span = document.createElement('span');
+    const raw = style.raw.reduce((prev, current, index) => {
+      return this.makeStyleString(arg, prev, current, index);
+    });
+    $span.setAttribute('style', raw);
+
+    return $span;
+  }
+
+  a(style, ...arg) {
+    const $a = document.createElement('a');
+    const raw = style.raw.reduce((prev, current, index) => {
+      return this.makeStyleString(arg, prev, current, index);
+    });
+    $a.setAttribute('style', raw);
+
+    return $a;
+  }
+
+  ul(style, ...arg) {
+    const $ul = document.createElement('ul');
+    const raw = style.raw.reduce((prev, current, index) => {
+      return this.makeStyleString(arg, prev, current, index);
+    });
+    $ul.setAttribute('style', raw);
+
+    return $ul;
+  }
+
+  li(style, ...arg) {
+    const $li = document.createElement('li');
+    const raw = style.raw.reduce((prev, current, index) => {
+      return this.makeStyleString(arg, prev, current, index);
+    });
+    $li.setAttribute('style', raw);
+
+    return $li;
+  }
+
   makeStyleString(arg, prev, current, index) {
     if (arg.length > 0) {
       return prev + arg[index - 1] + current
